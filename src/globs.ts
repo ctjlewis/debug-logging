fb9ad58cb9365100c4bbf633dd5484a5c361637f
C. Lewis <ctj.lewis@icloud.com>
2022-11-24 17:41:31 -0600

    v4.1.2

--- src/globs.ts
+++ src/globs.ts
@@ -6,17 +6,17 @@ const env: NodeJS.ProcessEnv = {
   NODE_ENV: "production",
 };
 
-/**
- * try ... catch around dynamic import so if this is executed in a browser
- * context it will not produce runtime failures.
- */
-try {
-  const { env: processEnv } = await import("process");
+if (typeof process !== "undefined") {
+  const { env: processEnv } = process;
+  /**
+   * try ... catch around dynamic import so if this is executed in a browser
+   * context it will not produce runtime failures.
+   */
   if (processEnv.NODE_ENV) {
     // console.log("Found NODE_ENV override", processEnv.NODE_ENV);
     env.NODE_ENV = processEnv.NODE_ENV;
   }
-} catch (e) {}
+}
 
 /**
  * Whether NODE_ENV is set to "development". Browser context will always be
